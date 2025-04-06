282faf8c6ce7332b48f75f2a98ff90c58b2431ba
Akash K <akash.kandasamy@zohocorp.com>
2025-04-06 20:17:48 +0530

    Tip Tap Editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./index.css";
+
+import 'highlight.js/styles/github.css';
 import Comp from "./Comp";
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import SidePanel from "./SidePanel";
@@ -7,12 +9,12 @@ function App() {
   return (
     <div className="main">
       <PanelGroup direction="horizontal" className="panel-group">
-        <Panel className="panel-1" minSize={30}>
+        <Panel className="panel-1" defaultSize={70}>
           <Comp />
         </Panel>
         <PanelResizeHandle />
     
-        <Panel defaultSize={40} minSize={30} className="panel-1">
+        <Panel defaultSize={30} minSize={30} maxSize={100} className="panel-1">
           <SidePanel />
         </Panel>
       </PanelGroup>
